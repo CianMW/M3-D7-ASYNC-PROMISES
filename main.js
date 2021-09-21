1554f1e84f231d16178c355e35ecec78900dd860
CianMW <markwick.cian@gmail.com>
2021-09-21 19:57:12 +0100

    update

--- main.js
+++ main.js
@@ -1,43 +1,24 @@
 
+const dataFetch = () => {
+   fetch ("https://jsonplaceholder.typicode.com/users")
+   .then(resp => resp.json())
+   .then(data => 
 
-const fetchedArray = []
-window.onload = () => {
-   fetchData()
-
-}
-
-
-const fetchData = async () =>  {
-   let response = await fetch ("https://jsonplaceholder.typicode.com/users")
-let data = await response.json()
-
-fetchedArray.push(data)
-console.log(fetchedArray)
-
-  
-
-//return data
+      console.log(data),
       
-    }
-    const mapped = fetchedArray.item.inputValue
-console.log(mapped)
-
-const newObject = Object.assign({}, ...fetchedArray.map(item => ({ [item.name]: item.value })))
-console.log(newObject)
+      )
+}
+dataFetch()
 
 
-    
-
-let inputValue = document.querySelector("select")
 
 
-inputValue.onchange = function(event){
+let selectLocation = document.querySelector("select")
 
-   let currentValue =  event.currentTarget.value
-   console.log(currentValue)
-    
+selectLocation.onchange = (event) => {
+   selectValue = event.currentTarget.value 
+
+   console.log(selectValue)
 }
 
-
-
-
+let selectValue = ""
