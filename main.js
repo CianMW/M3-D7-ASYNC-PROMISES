3c9e257eac0bd446ebce963c645ca8ece9b028bd
rashuhiremath <rashmi.hiremath94@gmail.com>
2021-09-21 15:55:26 +0200

    trying to fetch data

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 
-
+const fetchedArray = []
 
 
 
@@ -12,17 +12,27 @@ window.onload = () => {
 
 
 const fetchData = async () =>  {
-       await fetch ("https://jsonplaceholder.typicode.com/users")
-        .then(resp => resp.json())
-        .then(body =>
-            
-            console.log(body)
-    )}
+   let response = await fetch ("https://jsonplaceholder.typicode.com/users")
+let data = await response.json()
+
+fetchedArray.push(data)
+console.log(fetchedArray)
+
+  
+
+//return data
+      
+    }
+    const mapped = fetchedArray.item.inputValue
+console.log(mapped)
+
+const newObject = Object.assign({}, ...fetchedArray.map(item => ({ [item.name]: item.value })))
+console.log(newObject)
+
+
+    
 
 let inputValue = document.querySelector("select")
-
-
-
 
 
 inputValue.onchange = function(event){
