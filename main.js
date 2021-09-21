b8ad1293726ed0ae97ccf5c47b3d43a6004ea7de
CianMW <markwick.cian@gmail.com>
2021-09-21 22:02:40 +0100

    Question 1- list complete

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
 
+let dataArray 
+
+
 const dataFetch = () => {
    fetch ("https://jsonplaceholder.typicode.com/users")
    .then(resp => resp.json())
-   .then(data => 
+   .then(data => {
+      console.log(data)
 
-      console.log(data),
-      
-      )
+      dataArray = data
+      console.log(...dataArray)
+      addInformation()
+   })
+   
+
 }
+
 dataFetch()
+
+
+
+
 
 
 
@@ -22,3 +34,39 @@ selectLocation.onchange = (event) => {
 }
 
 let selectValue = ""
+
+let inputValue
+let inputLocation = document.querySelector("input")
+
+inputLocation.onchange = (event) => {
+   inputValue = event.currentTarget.value
+}
+
+
+let addInformation = () => {
+
+dataArray.forEach(obj => {
+
+   
+
+let name = obj.name
+
+let surname = obj.name
+
+let email = obj.name
+
+
+
+
+let newRowNode = document.createElement("tr")
+newRowNode.innerHTML = `        
+         <th scope="row">1</th>
+         <td>${name}</td>
+         <td>${surname}</td>
+         <td>${email}</td>
+         `
+
+document.querySelector("#data-table").appendChild(newRowNode)
+
+
+         })}
